dee4b6ffa77a6c324709e9d7f3b1525d177817b7
bsung03 <bsung@purdue.edu>
2021-05-29 16:32:04 -0500

    Product Details Redux

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants"
+import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants"
 
 export const listProducts = () => async (dispatch) => {
     dispatch({
@@ -15,6 +15,29 @@ export const listProducts = () => async (dispatch) => {
         dispatch({
             type: PRODUCT_LIST_FAIL,
             payload: error.message
+        });
+    }
+}
+
+export const detailsProduct = (productID) => async (dispatch) => {
+
+    dispatch({
+        type: PRODUCT_DETAILS_REQUEST,
+        payload: productID
+    });
+
+    try{
+        const { data } = await Axios.get(`/api/products/${productID}`);
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        });
+    }catch(error){
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
+            payload: error.response && error.response.data ? 
+                error.response.data.message :
+                error.message,
         });
     }
 }
