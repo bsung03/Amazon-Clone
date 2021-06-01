9d27162abe48b1fce495bbbfe60878cfb9df9ad3
bsung03 <bsung@purdue.edu>
2021-06-01 12:31:08 -0500

    seed mongo db and connect

--- backend/data.js
+++ backend/data.js
@@ -5,19 +5,18 @@ const data = {
     {
       name: 'Brandon',
       email: 'bsung20@gamil.com',
-      password : bcrypt.hashAsync('stephanie12', 8),
+      password : bcrypt.hashSync('stephanie12', 8),
       isAdmin: true,
     },
     {
       name: 'John',
       email: 'john@gamil.com',
-      password : bcrypt.hashAsync('john12', 8),
+      password : bcrypt.hashSync('john12', 8),
       isAdmin: false,
     },
   ],
     products: [
       {
-        _id: '1',
         name: 'Nike Slim Shirt',
         category: 'Shirts',
         image: '/images/product-1.jpg',
@@ -29,7 +28,6 @@ const data = {
         description: 'high quality product',
       },
       {
-        _id: '2',
         name: 'Adidas Fit Shirt',
         category: 'Shirts',
         image: '/images/product-1.jpg',
@@ -41,7 +39,6 @@ const data = {
         description: 'high quality product',
       },
       {
-        _id: '3',
         name: 'Lacoste Free Shirt',
         category: 'Shirts',
         image: '/images/product-1.jpg',
@@ -53,7 +50,6 @@ const data = {
         description: 'high quality product',
       },
       {
-        _id: '4',
         name: 'Nike Slim Pant',
         category: 'Pants',
         image: '/images/product-1.jpg',
@@ -65,7 +61,6 @@ const data = {
         description: 'high quality product',
       },
       {
-        _id: '5',
         name: 'Puma Slim Pant',
         category: 'Pants',
         image: '/images/product-1.jpg',
@@ -77,7 +72,6 @@ const data = {
         description: 'high quality product',
       },
       {
-        _id: '6',
         name: 'Adidas Fit Pant',
         category: 'Pants',
         image: '/images/product-1.jpg',
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,25 @@
 import express from 'express';
-import data from './data.js';
+import mongoose from 'mongoose';
+import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
 
 const app = express();
-
-app.get('/api/products/:id', (req,res) => {
-    const product = data.products.find((x) => x._id === req.params.id);
-    if(product){
-        res.send(product);
-    }else{
-        res.status(404).send({message: "Product Not Found"});
-    }
+// eslint-disable-next-line no-undef
+mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://Brandon:stephanie12@cluster0.gx035.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex:true
 });
 
-
-
-app.get('/api/products', (req,res) => {
-    res.send(data.products)
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
 })
 
+
+
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 
 
 app.get('/', (req,res) => {
@@ -25,9 +27,13 @@ app.get('/', (req,res) => {
 });
 
 
+// eslint-disable-next-line no-unused-vars
+app.use((err,req,res,next) => {
+    res.status(500).send({message: err.message})
+});
 
 
-
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
     console.log(`Server at http://localhost:${port}`)
