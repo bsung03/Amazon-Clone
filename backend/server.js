1654f2b76e93b24eb37211790987d091dbc2b5ba
bsung03 <bsung@purdue.edu>
2021-06-21 16:40:41 -0500

    heroku node env and npm commands

--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,15 @@ import orderRouter from './routers/orderRouter.js';
 dotenv.config()
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 
 
 // eslint-disable-next-line no-undef
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex:true
+    useCreateIndex: true
 });
 
 const connection = mongoose.connection;
@@ -30,26 +30,34 @@ connection.once('open', () => {
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
-app.use('/api/orders',orderRouter);
+app.use('/api/orders', orderRouter);
 
-app.get('/api/config/paypal' , (req,res) => {
+app.get('/api/config/paypal', (req, res) => {
     // eslint-disable-next-line no-undef
     res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send("Server is ready");
 });
 
 
 // eslint-disable-next-line no-unused-vars
-app.use((err,req,res,next) => {
-    res.status(500).send({message: err.message})
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message })
 });
 
 
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
-app.listen(port, ()=>{
+
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../frontend/build'))
+}
+
+
+
+app.listen(port, () => {
     console.log(`Server at http://localhost:${port}`)
 });
